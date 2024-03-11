3b5aeea27f26bb9b8188114ef4bad631210d6f51
Facundo Enriquez <fenriquez@inym.org.ar>
2024-03-11 13:55:19 -0300

    next auth with jws and session callbacks

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,7 +4,12 @@ import CredentialsProvider from 'next-auth/providers/credentials';
 const handler = NextAuth({
     providers: [
         CredentialsProvider({
+            // El nombre que se mostrará en el formulario de inicio de sesión (por ejemplo, "Iniciar sesión con...")
             name: 'Credentials',
+            // `credentials` se utiliza para generar un formulario en la página de inicio de sesión.
+            // Puede especificar qué campos deben enviarse agregando claves al objeto `credentials`.
+            // p.ej. dominio, nombre de usuario, contraseña, token 2FA, etc.
+            // Puedes pasar cualquier atributo HTML a la etiqueta <input> a través del objeto.
             credentials: {
                 email: { label: 'Email', type: 'email', placeholder: '' },
                 password: {
@@ -14,7 +19,7 @@ const handler = NextAuth({
                 },
             },
             async authorize(credentials, req) {
-                // Add logic here to look up the user from the credentials supplied
+                // Agregue lógica aquí para buscar el usuario a partir de las credenciales proporcionadas.
                 const user = {
                     id: '1',
                     name: 'J Smith',
@@ -22,29 +27,27 @@ const handler = NextAuth({
                 };
 
                 if (user) {
-                    // Any object returned will be saved in `user` property of the JWT
+                    // Cualquier objeto devuelto se guardará en la propiedad `usuario` del JWT
+                    console.log(user);
                     return user;
                 } else {
-                    // If you return null then an error will be displayed advising the user to check their details.
+                    // Si devuelve nulo, se mostrará un error advirtiendo al usuario que verifique sus detalles.
                     return null;
-
-                    // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+                    // También puedes rechazar esta devolución de llamada con un error, por lo que el usuario será enviado a la página de error con el mensaje de error como parámetro de consulta.
                 }
             },
         }),
     ],
     callbacks: {
         jwt({ account, token, user, profile, session }) {
-            console.log({
-                account,
-                token,
-                user,
-                profile,
-                session,
-            });
+            if (user) token.user = user;
             return token;
         },
-        // session() {},
+        session({ session, token }) {
+            session.user = token.user;
+            console.log(session, token);
+            return session;
+        },
     },
 });
 
